3a92b591ae4c224b7a2be460251c33eaa0a90e1e
proxer1982 <juanc.zorro@gmail.com>
2022-11-08 20:15:04 -0500

    esquemas de produto y carrito

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,39 @@
 import Mongo from "mongoose"
 
-Mongo.connect("mongodb+srv://developers:123456Aa@clusterprinc.crxbi6b.mongodb.net/developers3x1?retryWrites=true&w=majority")
+Mongo.connect("mongodb+srv://developers:123Rr@clusterprinc.crxbi6b.mongodb.net/developers3x1?retryWrites=true&w=majority")
 .then(() => console.log("Connexion con Mongo exitosa"))
 .catch((err) => console.log(err))
+
+let esquemaProd = Mongo.Schema({
+    _id:Number,
+    imagen:String,
+    cantidad:Number,
+    nombreProducto:String,
+    valor:Number,
+    descripcion:String
+}, {versionKey:false})
+
+let esquemaCarrito = Mongo.Schema({
+    _id:Number,
+    fecha:Date,
+    id_user:Number,
+    nombreProducto:Array
+}, {versionKey:false})
+
+
+let modeloProd = Mongo.model("productos", esquemaProd);
+
+let Insertar = async (datos) => {
+    console.log("guardando")
+    let documento = new modeloProd({
+        _id:4,
+        imagen:"imagen005.jpg",
+        cantidad:12,
+        nombreProducto:"juguete prueba",
+        valor:34555,
+        descripcion:"descripcion del juguete de prueba"
+    })
+    await documento.save()
+}
+
+export {Insertar}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import ex from "express"
-import "./db.js"
+import {Insertar} from "./db.js"
 
 const app = ex()
 app.listen("5050", function(){
     console.log("Servidor iniciado")
+})
+
+app.get("/agregar", function (pet, res){
+    console.log("agregando")
+    let datos = {datos:"prueba"}
+    Insertar(datos)
+    res.send("probando la url")
 })
